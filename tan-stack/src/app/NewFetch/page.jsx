b2539f6c3fd7f1d6716083ee1d47c448f7975be9
ShrivastvAryan <asguy29@gmail.com>
2025-08-15 16:18:13 +0530

    use-mutation

--- tan-stack/src/app/NewFetch/page.jsx
+++ tan-stack/src/app/NewFetch/page.jsx
@@ -3,20 +3,50 @@
 //Fetched using the tanStack Query
 
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import fetchPost from "../lib/api";
+import { deletePost } from "../lib/api";
+import { updatePost } from "../lib/api";
 
 const FetchNew = () => {
 
   const[pageNumber,setPageNumber]=useState(1);
+  const queryClient=useQueryClient();
   
   const { data, isLoading, error } = useQuery({
     queryKey: ["posts",pageNumber], // like useState for caching
     queryFn:()=> fetchPost(pageNumber), // like useEffect for fetching
+    placeholderData:keepPreviousData, //This will avoid showing loading text which makes page better
     //staleTime:10000, //This it will go stale after 5s which means till the time data remains fresh the data from server wont be fetched , it will be fetched from cache.
    // refetchInterval:1000, We can use either staleTime or refetchInterval
    //refetchIntervalBackground:true, by default it's but making it true will allow to refetch in the background also.
+  });
+
+
+  //mutation function to delete post
+  const deleteMutation = useMutation({
+    mutationFn: (id) => deletePost(id),
+    onSuccess: (data, id) => {
+      queryClient.setQueryData(["posts", pageNumber], (curElem) => {
+        return curElem?.filter((post) => post.id !== id);
+      });
+    },
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: (id) => updatePost(id),
+    onSuccess: (apiData, postId) => {
+      console.log(apiData, postId);
+
+      queryClient.setQueryData(["posts", pageNumber], (postsData) => {
+        return postsData?.map((curPost) => {
+          return curPost.id === postId
+            ? { ...curPost, title: apiData.data.title }
+            : curPost;
+        });
+      });
+    },
   });
 
   if (isLoading) {
@@ -31,9 +61,9 @@ const FetchNew = () => {
 
     <>
     <div className="flex gap-4 p-4">
-    <button onClick={()=>setPageNumber(prev=>prev-1)}>Prev</button>
-    <p>{pageNumber}</p>
-    <button onClick={()=>setPageNumber(prev=>prev+1)}>Next</button>
+    <button disabled={pageNumber===1 ? true:false} onClick={()=>setPageNumber(prev=>prev-3)}>Prev</button>
+    <p>{(pageNumber/3)}</p>
+    <button onClick={()=>setPageNumber(prev=>prev+3)}>Next</button>
     </div>
 
     <div>
@@ -45,6 +75,8 @@ const FetchNew = () => {
           
           <p className="text-2xl text-amber-700">{curElem.title}</p>
           <p>{curElem.body}</p>
+          <button onClick={()=>deleteMutation.mutate(curElem.id)}>Delete</button>
+          <button onClick={() => updateMutation.mutate(curElem.id)}>Update</button>
           
         </div>
       ))}
--- tan-stack/src/app/lib/api.jsx
+++ tan-stack/src/app/lib/api.jsx
@@ -15,3 +15,21 @@ export const fetchPost = async (pageNumber) => {
 };
 
 export default fetchPost
+
+export const fetchInvPost = async (id) => {
+  try {
+    const res = await api.get(`/posts/${id}`);
+    return res.status === 200 ? res.data : [];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost=(id)=>{
+    return api.delete(`/posts/${id}`)
+};
+
+/// to update the post
+export const updatePost = (id) => {
+  return api.patch(`/posts/${id}`, { title: "I have updated" });
+};
