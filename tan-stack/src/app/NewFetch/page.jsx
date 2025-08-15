cd0aef7114021251371d90923d94909c6ccdc631
ShrivastvAryan <asguy29@gmail.com>
2025-08-15 13:04:35 +0530

    pagination

--- tan-stack/src/app/NewFetch/page.jsx
+++ tan-stack/src/app/NewFetch/page.jsx
@@ -3,27 +3,20 @@
 //Fetched using the tanStack Query
 
 import React from "react";
-import fetchPost from "../lib/api";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import fetchPost from "../lib/api";
 
 const FetchNew = () => {
-  const getPostsData = async () => {
-    try {
-      const res = await fetchPost();
-      if (res.status === 200) {
-        return res.data; // ✅ return the data so React Query stores it
-      }
-      return [];
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
-  };
 
+  const[pageNumber,setPageNumber]=useState(1);
+  
   const { data, isLoading, error } = useQuery({
-    queryKey: ["posts"], // like useState for caching
-    queryFn: getPostsData, // like useEffect for fetching
-    staleTime:5000, //This it will go stale after 5s
+    queryKey: ["posts",pageNumber], // like useState for caching
+    queryFn:()=> fetchPost(pageNumber), // like useEffect for fetching
+    //staleTime:10000, //This it will go stale after 5s which means till the time data remains fresh the data from server wont be fetched , it will be fetched from cache.
+   // refetchInterval:1000, We can use either staleTime or refetchInterval
+   //refetchIntervalBackground:true, by default it's but making it true will allow to refetch in the background also.
   });
 
   if (isLoading) {
@@ -35,17 +28,29 @@ const FetchNew = () => {
   }
 
   return (
+
+    <>
+    <div className="flex gap-4 p-4">
+    <button onClick={()=>setPageNumber(prev=>prev-1)}>Prev</button>
+    <p>{pageNumber}</p>
+    <button onClick={()=>setPageNumber(prev=>prev+1)}>Next</button>
+    </div>
+
     <div>
       {data?.map((curElem) => (
         <div
           key={curElem.id}
           className="w-auto h-auto p-2 border border-amber-100 rounded-2xl flex flex-col gap-2 my-4"
         >
+          
           <p className="text-2xl text-amber-700">{curElem.title}</p>
           <p>{curElem.body}</p>
+          
         </div>
       ))}
     </div>
+
+    </>
   );
 };
 
--- tan-stack/src/app/lib/api.jsx
+++ tan-stack/src/app/lib/api.jsx
@@ -4,8 +4,14 @@ const api =axios.create({
     baseURL:'https://jsonplaceholder.typicode.com'
 });
 
-const fetchPost=()=>{
-    return api.get('/posts')
-}
+// to fetch the data
+export const fetchPost = async (pageNumber) => {
+  try {
+    const res = await api.get(`/posts?_start=${pageNumber}&_limit=3`);
+    return res.status === 200 ? res.data : [];
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default fetchPost
